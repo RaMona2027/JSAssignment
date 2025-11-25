8ad3c852e48f368d391cca057ff211af54a08853
RaMona2027 <ramona.dusey@gmail.com>
2025-11-25 10:28:04 +0100

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+console.log("this is line 1");
+console.log("this is line 2");
+console.log("this is line 3"); console.log("this is line 4");
+console.log("this is line 5");;
+;
+console.log("this is line 6");
